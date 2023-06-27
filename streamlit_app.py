5844abb78c0baf3f9f442ad656e57e5676361e93
asish34 <63844544+asish34@users.noreply.github.com>
2023-06-27 14:32:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
-streamlit.title("Hello MY name is asish maity")
+streamlit.title("My parents new healthy dinner")
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega 3 and blueberry ota meal")
+steramlit.text("Kale ,spinach and rocket smoothy")
+streamlit.text("Hard boiled and free range egg")
